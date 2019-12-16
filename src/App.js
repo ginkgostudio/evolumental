84518e3ff4615a08cf12486ea7bcb735f7eeb306
Natalia Varela <natalia.varela01@gmail.com>
2019-12-16 19:57:11 -0300

    Dashboard route within Switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard'
 
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar />  
+        <Switch>
+          <Route path='/' component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
