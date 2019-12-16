53c3ce49c01691c36369158bffd343cbdab32732
Natalia Varela <natalia.varela01@gmail.com>
2019-12-16 19:57:34 -0300

    Placeholder cards for upcoming appointments

--- src/components/appointments/UpcomingAppointments.js
+++ src/components/appointments/UpcomingAppointments.js
@@ -2,7 +2,43 @@ import React from 'react'
 
 const UpcomingAppointments = () => {
     return (
-        <p>hello world</p>
+        <div className="upcoming-appointments section">
+            <div className="card teal lighten-4 z-depth-5">
+                <div className="card-content-grey-text text-darken-3">
+                    <span className="card-title">Patient Name</span>
+                    <p>Appointment reason</p>
+                    <p className="grey-text">3rd September, 2am</p>
+                </div>
+            </div>
+            <div className="card teal lighten-4 z-depth-1">
+                <div className="card-content-grey-text text-darken-3">
+                    <span className="card-title">Patient Name</span>
+                    <p>Appointment reason</p>
+                    <p className="grey-text">3rd September, 2am</p>
+                </div>
+            </div>
+            <div className="card teal lighten-4 z-depth-1">
+                <div className="card-content-grey-text text-darken-3">
+                    <span className="card-title">Patient Name</span>
+                    <p>Appointment reason</p>
+                    <p className="grey-text">3rd September, 2am</p>
+                </div>
+            </div>
+            <div className="card teal lighten-4 z-depth-1">
+                <div className="card-content-grey-text text-darken-3">
+                    <span className="card-title">Patient Name</span>
+                    <p>Appointment reason</p>
+                    <p className="grey-text">3rd September, 2am</p>
+                </div>
+            </div>
+            <div className="card teal lighten-4 z-depth-1">
+                <div className="card-content-grey-text text-darken-3">
+                    <span className="card-title">Patient Name</span>
+                    <p>Appointment reason</p>
+                    <p className="grey-text">3rd September, 2am</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
