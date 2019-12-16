50a1326f1b589049aa15102052bb7c1e5039f37c
Natalia Varela <natalia.varela01@gmail.com>
2019-12-16 17:50:43 -0300

    import links to navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import SignedInLinks from './SignedInLinks'
+import SignedOutLinks from './SignedOutLinks'
+
 
 const Navbar = () => {
     return (
-        
-    )
+        <nav className="nav-wrapper blue darken-3">
+            <div className="container">
+                <Link to="/" className="brand-logo">Evolumental</Link>
+                <SignedInLinks />
+                <SignedOutLinks />
+
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar
