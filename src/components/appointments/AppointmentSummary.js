04b19da279e42a6c269b0e14957c4a46221a6093
Natalia Varela <natalia.varela01@gmail.com>
2019-12-17 20:46:49 -0300

    Module for Appointment Summary and nesting in Upcoming Appointments listing

---
+++ src/components/appointments/AppointmentSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AppointmentSummary = () => {
+    return (
+        <div className="card teal lighten-4 z-depth-5">
+                <div className="card-content-grey-text text-darken-3">
+                    <span className="card-title">Patient Name</span>
+                    <p>Appointment reason</p>
+                    <p className="grey-text">3rd September, 2am</p>
+                </div>
+        </div>
+    )
+}
+
+export default AppointmentSummary
--- src/components/appointments/UpcomingAppointments.js
+++ src/components/appointments/UpcomingAppointments.js
@@ -1,43 +1,13 @@
 import React from 'react'
+import AppointmentSummary from './AppointmentSummary'
 
 const UpcomingAppointments = () => {
     return (
         <div className="upcoming-appointments section">
-            <div className="card teal lighten-4 z-depth-5">
-                <div className="card-content-grey-text text-darken-3">
-                    <span className="card-title">Patient Name</span>
-                    <p>Appointment reason</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-            <div className="card teal lighten-4 z-depth-1">
-                <div className="card-content-grey-text text-darken-3">
-                    <span className="card-title">Patient Name</span>
-                    <p>Appointment reason</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-            <div className="card teal lighten-4 z-depth-1">
-                <div className="card-content-grey-text text-darken-3">
-                    <span className="card-title">Patient Name</span>
-                    <p>Appointment reason</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-            <div className="card teal lighten-4 z-depth-1">
-                <div className="card-content-grey-text text-darken-3">
-                    <span className="card-title">Patient Name</span>
-                    <p>Appointment reason</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-            <div className="card teal lighten-4 z-depth-1">
-                <div className="card-content-grey-text text-darken-3">
-                    <span className="card-title">Patient Name</span>
-                    <p>Appointment reason</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
+            <AppointmentSummary />
+            <AppointmentSummary />
+            <AppointmentSummary />
+            <AppointmentSummary />
         </div>
     )
 }
