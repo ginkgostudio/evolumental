dbddacc809df092649eadfaa908f28f80cb1f596
Natalia Varela <natalia.varela01@gmail.com>
2019-12-18 14:23:10 -0300

    Create Account component & route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard'
 import SignIn from './components/authentication/SignIn'
+import CreateAccount from './components/authentication/CreateAccount'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Dashboard} />
           <Route path='/signin' component={SignIn} />
+          <Route path='/createaccount' component={CreateAccount} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/authentication/CreateAccount.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+class CreateAccount extends Component {
+    state = {
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        position: '',
+        profLicense: ''
+
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+    }
+    render () {
+        return (
+            <div className="container">
+                <form onSubmit={this.handleSubmit} className="white">
+                    <div className="input-field">
+                        <label htmlFor ="email">Email</label>
+                        <input type="email" id="email" onChange={this.handleChange}></input>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor ="password">Password</label>
+                        <input type="password" id="password" onChange={this.handleChange}></input>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor ="firstName">First Name</label>
+                        <input type="text" id="firstName" onChange={this.handleChange}></input>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor ="lastName">Last Name</label>
+                        <input type="text" id="lastName" onChange={this.handleChange}></input>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor ="position">Position / degree</label>
+                        <input type="text" id="position" onChange={this.handleChange}></input>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor ="profLicense">Professional License</label>
+                        <input type="text" id="profLicense" onChange={this.handleChange}></input>
+                    </div>
+
+                    <button className="btn blue lightn-1 z-depth-0">Create Account</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default CreateAccount
